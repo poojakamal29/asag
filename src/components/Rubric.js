0e028acd42052ebdda1761e15bdb0b067b4bbc04
poojakamal29 <71355837+poojakamal29@users.noreply.github.com>
2021-09-28 09:55:48 -0500

    Update Rubric.js

--- src/components/Rubric.js
+++ src/components/Rubric.js
@@ -14,7 +14,14 @@ function Rubric() {
         <div className='rubric__wrapper'>
           <ul className='rubric__items'>
             <textarea>
-   
+            Mr. Kelly Kort is a previously healthy 45 y.o. man who presents to the ambulatory clinic ( day 1) with acute back pain. Three weeks ago was moving
+boxes in his living room and after which felt back pain immediately. The pain is described as diffuse and non-radiating. The pain however feels sharp
+when he makes quick moves. He is concerned because the pain has not gotten better and the pain is the worst back pain that he's ever felt. He takes
+tylenol 2-3 times a day and it has not gotten better or gotten worse. Nothing makes the pain better. He is also concerned because his dad died of
+prostate cancer with mets to the spine and he is concerned that he may have cancer of his spine. He drinks a couple times a week but denies drugs and
+excessive exercise. He denies flu like symptoms, weight loss, blood loss, dysphagia, and chills, n/v, or syncope. He is otherwise well. Differential
+diagnosis for this includes: spinal compression fracture most likely, sprain, or much lower spinal stenosis, pyelonephritis, or compression due to tumor.
+Unlikely however cancer but still can proceed with imaging to rule out.
             </textarea>
           </ul>
 
@@ -22,39 +29,63 @@ function Rubric() {
           <ul className='credit'>
             <h3> Items Found in Note: </h3>
           <RubricItem 
-              text='PPNBP3WEEKS'
+              text='Back pain for 3 weeks described as dull ache exacerbated by any movement'
             />
                       <RubricItem 
-              text='PPNFURNITURE'
+              text='Began after moving furniture'
             />
                       <RubricItem 
-              text='PPNFLEX'
+              text='Worse with flexion and rotation'
             />
                       <RubricItem 
-              text='PPNTYLENOL'
+              text='Trying Motrin and Tylenol with slight relief'
             />
                       <RubricItem 
-              text='PPNRADIATE'
+              text=' Pain radiates laterally across the lower back'
             />
                                   <RubricItem 
-              text='PPNEXCERCISE'
+              text=' Unable to perform usual exercise routine'
             />
                       <RubricItem 
-              text='PPNFATHERHIST'
+              text='Father with history of prostate cancer with spine mets'
             />
                       <RubricItem 
-              text='PPNLEGS'
+              text='No radiation to the legs'
             />
                       <RubricItem 
-              text='PPNBOWEL'
+              text='No bowel symptoms'
             />
+            
+            <RubricItem 
+              text='No LE weakness, numbness or tingling'
+            />
+                        <RubricItem 
+              text=' No fever'
+            />
+                        <RubricItem 
+              text='No past medical history of note'
+            />
+                        <RubricItem 
+              text='No IV drug use in the past'
+            />
+                        <RubricItem 
+              text=' No smoking in the past'
+            />
+                        <RubricItem 
+              text='No concerning alcohol use'
+            />
+             
+
 
           </ul>
 
           <ul className='nocredit'>
           <h3> Items NOT Found in Note: </h3>
           <RubricItem
-              text='PPNBLADER'
+              text='No bladder symptoms'
+            />
+                       <RubricItem 
+              text='No weight loss'
             />
  
           </ul>
@@ -67,15 +98,36 @@ function Rubric() {
         <div className='rubric__wrapper'>
           <ul className='rubric__items'>
             <textarea>
-            No spinal tenderness to palpation. Paraspinal muscles in lumbar region tender to palpation bilaterally. Limited spinal flexion, lateral bending. 
-            </textarea>
+            12 point exam was negative with the exception of musculoskeletal findings. Including negative for lymphadenopathy. Patient had a positive straight leg
+            test. Was able to move the leg only 60 degrees. Had difficulty with flexion of hips bilaterally. Difficulty with spine flexion. Denies flank pain.            </textarea>
           </ul>
 
           <h1> Physical Exam Evaluation </h1>
           <ul className='credit'>
             <h3> Items Found in Note: </h3>
           <RubricItem 
-              text=''
+              text=' Vital signs mentioned - normal'
+            />
+                      <RubricItem 
+              text='Pain on paraspinal palpation'
+            />
+                      <RubricItem 
+              text='Motor strength 5/5 in LE'
+            />
+                      <RubricItem 
+              text=' Reflexes 2/4 in LE bilaterally'
+            />
+                                  <RubricItem 
+              text=' Sensory intact in LE bilaterally'
+            />
+                                  <RubricItem 
+              text=' Pulses 2/4 in DP/PT'
+            />
+                                  <RubricItem 
+              text=' No vertebral tenderness'
+            />
+                                  <RubricItem 
+              text='Babinski down going bilaterally'
             />
 
           </ul>
@@ -83,10 +135,8 @@ function Rubric() {
           <ul className='nocredit'>
           <h3> Items NOT Found in Note: </h3>
           <RubricItem
-              text=''
+              text='Straight leg raise'
               />
-
-
           </ul>
         </div>
       </div>
@@ -100,6 +150,9 @@ function Rubric() {
           </Link>
         </div>
     </div>
+
+
+
   );
 }
 
