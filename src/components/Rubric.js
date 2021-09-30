989be83632c7526875f63a23f185f9190c9fcb01
poojakamal29 <71355837+poojakamal29@users.noreply.github.com>
2021-09-30 11:02:56 -0500

    perfect rubric

--- src/components/Rubric.js
+++ src/components/Rubric.js
@@ -9,15 +9,17 @@ import { Link } from 'react-router-dom';
 function Rubric() {
   return (
     <div className='rubric'>
-      <h1>Your History Answer</h1>
+      <h2 className = 'title'> Overall Learner Evaluation</h2>
       <div className='rubric__container'>
         <div className='rubric__wrapper'>
+          <h1>Your History Answer</h1>
+
+
           <ul className='rubric__items'>
-            <textarea className = 'textbox'>
-            '-Lower back pain for 3 weeks since rearranging furniture -constant pain on both sides of lumbrosacral back, 8/10 -worsens with movement -able to walk -
-no weakness, numbness, parasthesias -Motrin, Tylenol provided limited relief -Recent prostate exam 6 mo ago - normal -Family history of prostate
-cancer with mets to spine in father
-            </textarea>
+
+            <RubricItem 
+              text = ""
+            />
           </ul>
 
           <h1> History Evaluation </h1>
@@ -25,96 +27,88 @@ cancer with mets to spine in father
 
           <ul className='credit'>
             <h3> Items Found in Note: </h3>
-            <RubricItem className = 'highconfidence'
+          <RubricItem 
               text='Back pain for 3 weeks described as dull ache exacerbated by any movement *'
-            />
-                      <RubricItem 
+          />
+          <RubricItem 
               text='Began after moving furniture *'
-            />
-
-                      <RubricItem 
+          />
+          <RubricItem 
+              text='Worse with flexion and rotation'
+          />
+          <RubricItem 
               text='Trying Motrin and Tylenol with slight relief *'
-            />
-
-                                  <RubricItem 
+          />
+          <RubricItem 
+              text=' Pain radiates laterally across the lower back'
+          />
+          <RubricItem 
               text=' Unable to perform usual exercise routine'
-            />
-                      <RubricItem 
+          />
+          <RubricItem 
               text='Father with history of prostate cancer with spine mets'
             />
-                      <RubricItem 
+          <RubricItem 
               text='No radiation to the legs'
-            />
-                      <RubricItem 
+          />
+          <RubricItem 
               text='No bowel symptoms'
-            />
-            
-            <RubricItem 
-              text='No LE weakness, numbness or tingling *'
-            />
-                        <RubricItem 
-              text=' No fever *'
-            />
-                        <RubricItem 
-              text='No past medical history of note'
-            />
-                        <RubricItem 
-              text='No IV drug use in the past'
-            />
-                        <RubricItem 
-              text=' No smoking in the past'
-            />
-                        <RubricItem 
-              text='No concerning alcohol use'
-            />
-             
+          />
 
+          <RubricItem
+              text='No bladder symptoms'
+          />
+            
+          <RubricItem 
+              text='No LE weakness, numbness or tingling *'
+          />
+          <RubricItem 
+              text=' No fever *'
+          />
+          <RubricItem 
+              text='No past medical history of note'
+          />
+          <RubricItem 
+              text='No IV drug use in the past'
+          />
+          <RubricItem 
+              text=' No smoking in the past'
+          />
+          <RubricItem 
+              text='No concerning alcohol use'
+          />
+          <RubricItem 
+              text='No weight loss'
+          />             
 
           </ul>
 
           <ul className='nocredit'>
           <h3> Items NOT Found in Note: </h3>
 
-                                  <RubricItem 
-              text='Worse with flexion and rotation'
-            />
-          <RubricItem
-          
-              text='No bladder symptoms'
-            />
-                              <RubricItem 
-              text='No radiation to the legs'
-            />
-                      <RubricItem 
-              text='No bowel symptoms'
-            />
-                                  <RubricItem 
-              text=' No fever *'
-            />
-                                  <RubricItem 
-              text='No concerning alcohol use'
-            />
 
-        <RubricItem 
-              text=' Pain radiates laterally across the lower back'
-            />
-
-                       <RubricItem 
-              text='No weight loss'
-            />
  
           </ul>
         </div>
       </div>
 
 
-      <h1>Your Physical Exam Answer</h1>
+
+
+
+
+
+
+
       <div className='rubric__container'>
         <div className='rubric__wrapper'>
+        <h1>Your Physical Exam Answer</h1>
+
+
           <ul className='rubric__items'>
-            <textarea>
-            '-bilateral paraspinal tenderness -No vertebral tenderness -straight leg test negative            
-            </textarea>
+            <RubricItem
+            text = ''           
+            />
           </ul>
 
           <h1> Physical Exam Evaluation </h1>
@@ -122,75 +116,86 @@ cancer with mets to spine in father
 
           <ul className='credit'>
             <h3> Items Found in Note: </h3>
-
-                      <RubricItem 
+          <RubricItem 
+              text='Vital signs mentioned - normal'
+          />
+          <RubricItem 
               text='Pain on paraspinal palpation *'
-            />
-
-                                  <RubricItem 
+          />
+          <RubricItem 
+              text='Motor strength 5/5 in LE *'
+          />
+          <RubricItem 
+              text=' Reflexes 2/4 in LE bilaterally'
+          />
+          <RubricItem 
+              text=' Sensory intact in LE bilaterally'
+          />
+          <RubricItem 
+              text=' Pulses 2/4 in DP/PT'
+          />
+          <RubricItem 
+              text='Limited range of motion of the back'
+          />
+          <RubricItem 
               text=' No vertebral tenderness *'
-            />
-                                  <RubricItem 
+          />
+          <RubricItem 
               text='Babinski down going bilaterally'
-            />
-
+          />
+          <RubricItem
+              text='Straight leg raise'
+          />
           </ul>
 
           <ul className='nocredit'>
           <h3> Items NOT Found in Note: </h3>
-          <RubricItem 
-              text='Vital signs mentioned - normal'
-            />
-          <RubricItem
-              text='Straight leg raise'
-              />
-                                <RubricItem 
-              text='Motor strength 5/5 in LE *'
-            />
-                      <RubricItem 
-              text=' Reflexes 2/4 in LE bilaterally'
-            />
-                                  <RubricItem 
-              text=' Sensory intact in LE bilaterally'
-            />
-                                  <RubricItem 
-              text=' Pulses 2/4 in DP/PT'
-            />
-                                  <RubricItem 
-              text='Limited range of motion of the back'
-            />
-            
+
           </ul>
         </div>
       </div>
 
 
 
+
+
   
 
-      <h1>Your Diagnosis Answer</h1>
       <div className='rubric__container'>
         <div className='rubric__wrapper'>
+        <h1>Your Differential Diagnosis Answer</h1>
+        <h4 className = 'black'> Note: We are looking for the items faculty suggested to consider in the differential diagnosis, though diagnoses may be ruled out through history and physical examination sections only. </h4>
+
+
           <ul className='rubric__items'>
-            <textarea>
-            Lumbar sprain
-            </textarea>
-
-            <textarea>
-            paraspinal tenderness, no vertebral tenderness
-            </textarea>
-
-            <textarea>
-            FHx of spinal mets. Unlikely due to good health.
-            </textarea>
+            <RubricItem
+            text = ''
+            />
           </ul>
 
-          <h1> Diagnosis Evaluation </h1>
+          <h1> Actual Patient Diagnosis </h1>
+          <ul className='rubric__items'>
+            <RubricItem
+            text =  'Lumbar muscular strain'
+            />
+          </ul>
+
+
+          <h1> Differential Diagnosis Evaluation </h1>
           <h4> Note: Any item marked with an * displays an item where the machine had high confidence </h4>
 
           <ul className='credit'>
             <h3> Items Found in Note: </h3>
-          <RubricItem 
+
+            <RubricItem
+              text='Low back strain'
+            />    
+
+            <RubricItem
+              text='Lumbar Disc Disease'
+            />          
+
+            <RubricItem 
               text='Cancer with spinal mets'
             />
 
@@ -198,80 +203,58 @@ cancer with mets to spine in father
 
           <ul className='nocredit'>
           <h3> Items NOT Found in Note: </h3>
-          <RubricItem
-              text='Low back strain'
-              />
-                    <RubricItem
-              text='Lumbar Disc Disease'
-              />
+
+
           </ul>
         </div>
       </div>
 
 
 
-      <h1>Your Test Ordered Answer</h1>
+
+
+
+
       <div className='rubric__container'>
         <div className='rubric__wrapper'>
+        <h1> Your "Tests Ordered" Answer(s)</h1>
+        <h4 className = 'black'> Note: For this section "tests ordered" there will not be an evaluation portion on the answer submitted, instead there is a display of your answer and the correct answer. 
+
+        </h4>
+        <ul className='rubric__items'>
+          <RubricItem className = 'length'
+              text=''
+          />
+          <RubricItem 
+              text=''
+          />
+          <RubricItem 
+              text=''
+          />
+          <RubricItem 
+              text=''
+          />
+          <RubricItem 
+              text=''
+          />          
+          </ul>
+
+
+          <h1>Correct Tests Indicated for Case</h1>
           <ul className='rubric__items'>
-            <textarea>
-            No further testing.
-            </textarea>
-
-            <textarea>
-            No Answer
-            </textarea>
-
-            <textarea>
-              No Answer
-            </textarea>
-
-            <textarea>
-              No Answer
-            </textarea>
-
-            <textarea>
-              No Answer
-            </textarea>
+            <RubricItem
+                text = 'No Testing Indicated'
+              />
           </ul>
 
-          <h1> Test Ordered Evaluation </h1>
-          <h4> Note: Any item marked with an * displays an item where the machine had high confidence </h4>
           <ul className='credit'>
-            <h3> Items Found in Note: </h3>
-          <RubricItem text = 'None '/>
 
-          </ul>
-
-          <ul className='nocredit'>
-          <h3> Items NOT Found in Note: </h3>
-          <RubricItem 
-              text='MRI'
-            />
-          <RubricItem 
-              text='PSA'
-            />
-          <RubricItem
-              text='Prostate exam'
-              />
-          <RubricItem
-              text='XRAY'
-              />
-
-          <RubricItem
-              text='CT'
-              />
-
-          <RubricItem
-              text='PSAPE'
-              />
-
-          <RubricItem
-              text='None'
-              />
           </ul>
         </div>
       </div>
+
+
+
 
 
 
@@ -280,6 +263,7 @@ cancer with mets to spine in father
           <Link to={{pathname: 'https://jumpsimulation.sjc1.qualtrics.com/jfe/form/SV_6Xcf6pMBNOLAEFU'}} target="_blank">
             <Button
               buttonStyle='btn--test'
+              buttonSize = 'btn--large'
             >
               Finish & Take Survey
             </Button>
